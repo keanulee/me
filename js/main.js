4ad09845232af6af26f9fb82efe1f9948419ab12
Keanu Lee <keanulee517@gmail.com>
2013-08-26 01:43:24 -0600

    Parallax scrolling effect

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-	$("footer .social-icons a").tooltip()
-})
+	$("footer .social-icons a").tooltip();
+
+	$(window).scroll(function () {
+    $(".splash").css("background-position-y", "-" + ($(this).scrollTop() / 2) + "px");
+	});
+});
