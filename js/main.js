ae25318ea3ac3ae87f0182a5a9cfbb5fbe7f58b6
Keanu Lee <keanulee517@gmail.com>
2013-09-05 16:44:21 -0600

    IE fixes

--- js/main.js
+++ js/main.js
@@ -4,13 +4,4 @@ $(document).ready(function() {
 	$(window).scroll(function () {
     $(".splash").css("background-position", "right " + ($(this).scrollTop() / 2) + "px");
 	});
-
-	// $(document).on("touchmove", function (e) {
-	// 	e.preventDefault();
-	// 	$(document).scrollTop($(this).scrollTop()+1);
-	// });
-
-	// document.addEventListener("touchmove", function () {
- //    $(".splash").css("background-position", "right " + ( / 2) + "px");
-	// }, false);
 });
