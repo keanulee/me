3de9ca4e20c8a90dabf287b5c2e05fa3f7fec0e6
Keanu Lee <keanulee517@gmail.com>
2013-09-05 15:18:55 -0600

    New splash image

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ $(document).ready(function() {
 	$("footer .social-icons a").tooltip();
 
 	$(window).scroll(function () {
-    $(".splash").css("background-position-y", "-" + ($(this).scrollTop() / 2) + "px");
+    $(".splash").css("background-position", "right " + ($(this).scrollTop() / 2) + "px");
 	});
+
+	// $(document).on("touchmove", function (e) {
+	// 	e.preventDefault();
+	// 	$(document).scrollTop($(this).scrollTop()+1);
+	// });
+
+	// document.addEventListener("touchmove", function () {
+ //    $(".splash").css("background-position", "right " + ( / 2) + "px");
+	// }, false);
 });
